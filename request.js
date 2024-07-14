c8742ac9d1aee36c879743662a5c24668f81df73
Jasper Mayone <me@jaspermayone.com>
2024-07-14 11:54:15 -0400

    raw request

---
+++ request.js
@@ -0,0 +1,18 @@
+fetch("https://WORKSPACEURL.slack.com/api/conversations.convertToPublic?_x_id=f0e13e8c-1720972125.659&_x_csid=Z8crK29Wpkk&slack_route=T07986PHP2R&_x_version_ts=1720955557&_x_frontend_build_type=current&_x_desktop_ia=4&_x_gantry=true&fp=51&_x_num_retries=0", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryRTCuJwNJLmMlBuM8",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "cookie": "d-s=1719083763; b=.86d1624b8fc2401a26a1d0240464f600; tz=-240; shown_ssb_redirect_page=1; shown_download_ssb_modal=1; show_download_ssb_banner=1; no_download_ssb_banner=1; lc=1719326292; ssb_instance_id=23258371-ac3b-4ebf-93b4-695ab7482ae0; OptanonConsent=isGpcEnabled=0&datestamp=Sun+Jul+14+2024+11%3A30%3A44+GMT-0400+(Eastern+Daylight+Time)&version=202402.1.0&browserGpcFlag=0&isIABGlobal=false&hosts=&consentId=4fec9100-0d0f-4de4-be54-9d0817d64451&interactionCount=1&isAnonUser=1&landingPath=NotLandingPage&groups=1%3A1%2C3%3A1%2C2%3A1%2C4%3A0&AwaitingReconsent=false; x=86d1624b8fc2401a26a1d0240464f600.1720972073; d=XOXD; web_cache_last_updated6a785dc46a2c4e6b610edb6c2d1987a0=1720972076606"
+  },
+  "referrerPolicy": "no-referrer",
+  "body": "------WebKitFormBoundaryRTCuJwNJLmMlBuM8\r\nContent-Disposition: form-data; name=\"token\"\r\n\r\nXOXC HERE\r\n------WebKitFormBoundaryRTCuJwNJLmMlBuM8\r\nContent-Disposition: form-data; name=\"channel\"\r\n\r\nC07C9Q76JMT\r\n------WebKitFormBoundaryRTCuJwNJLmMlBuM8\r\nContent-Disposition: form-data; name=\"_x_reason\"\r\n\r\nconvert_to_public\r\n------WebKitFormBoundaryRTCuJwNJLmMlBuM8\r\nContent-Disposition: form-data; name=\"_x_mode\"\r\n\r\nonline\r\n------WebKitFormBoundaryRTCuJwNJLmMlBuM8\r\nContent-Disposition: form-data; name=\"_x_sonic\"\r\n\r\ntrue\r\n------WebKitFormBoundaryRTCuJwNJLmMlBuM8\r\nContent-Disposition: form-data; name=\"_x_app_name\"\r\n\r\nclient\r\n------WebKitFormBoundaryRTCuJwNJLmMlBuM8--\r\n",
+  "method": "POST"
+});
